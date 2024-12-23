5e865d5c6d44e0fb5f91a703967c2a5a04318aac
SongyosR <songyos.r@psu.ac.th>
2024-12-23 11:31:31 +0700

    update vignette

---
+++ R/spax-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom raster raster
+#' @importFrom stats rbinom
+#' @importFrom stats rnbinom
+#' @importFrom stats rpois
+#' @importFrom stats setNames
+## usethis namespace: end
+NULL
