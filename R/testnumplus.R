d94099c8cbd9edf0546b3336d4a13fb35199d979
SongyosR <songyos.r@psu.ac.th>
2025-01-01 15:04:25 +0700

    test numplus

--- R/testnumplus.R
+++ R/testnumplus.R
@@ -1,10 +1,13 @@
+#' Create nnumplus
 #' @export
 new_numplus <- function(x = double()) {
   stopifnot(is.double(x))
   structure(x + 1, class = c("numplus", "numeric"))
 }
 
+#' Calculate mean for numplus objects
 #' @export
+#' @method mean numplus
 mean.numplus <- function(x, ...) {
   # Ensure x is treated as a numeric vector for the base mean function
   base_mean <- base::mean(as.numeric(x), ...)
