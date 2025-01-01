cd20cb897cec1caf9cae00461464a3562b11f57b
SongyosR <songyos.r@psu.ac.th>
2025-01-01 14:09:31 +0700

    test s3 dispatch

---
+++ R/testnumplus.R
@@ -0,0 +1,14 @@
+#' @export
+new_numplus <- function(x = double()) {
+  stopifnot(is.double(x))
+  structure(x + 1, class = c("numplus", "numeric"))
+}
+
+#' @export
+mean.numplus <- function(x, ...) {
+  # Ensure x is treated as a numeric vector for the base mean function
+  base_mean <- base::mean(as.numeric(x), ...)
+  # Add 50 to the result
+  return(base_mean + 50)
+}
+
